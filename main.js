44dca62d8d54028753cc2bffd70847f5fd421a7e
EC2 Default User <ec2-user@ip-172-31-11-39.us-west-1.compute.internal>
2022-08-09 04:59:55 +0000

    電卓

--- main.js
+++ main.js
@@ -1,12 +1,54 @@
-const result = document.getElementById("result");
-function edit(value) {
-  result.value = result.value + value;
+
+var result = "";
+var is_calc = false;
+
+// 初期表示
+window.onload = function () {
+  result = document.getElementById('result');
+};
+
+function c_click(){
+  result.value = "0";
+  is_calc = false;
 }
-function calc() {
-  result.value = new Function(
-    "return " + result.value
-  )();
+// 数字キー押下
+function num_click(val){
+  if(is_calc)  result.value = "0";
+  is_calc = false;
+
+  if(result.value =="0" && val == "0"){
+    result.value = "0";
+  }else if(result.value == "0" && val == "."){
+    result.value = "0.";
+  }else if(result.value == "0"){
+    result.value = val;
+  }else{
+    result.value += val;
+  }
 }
-function reset() {
-  result.value = "";
+
+function ope_click(val){
+  if(is_calc)  is_calc = false;
+
+  if(is_ope_last()){
+    result.value = result.value.slice(0, -1) + val;
+  } else {
+    result.value += val;
+  }
+}
+
+// =キークリック
+function equal_click(){
+  if(is_ope_last())  result.value = result.value.slice(0, -1);
+
+  var temp = new Function("return " + result.value.replaceAll("×", "*").replaceAll("÷", "/"))();
+  if(temp == Infinity || Number.isNaN(temp)){
+    result.value = "Error";
+  }else{
+    result.value = temp;
+    is_calc = true;
+  }
+}
+function is_ope_last(){
+  return ["+","-","×","÷",].includes(result.value.toString().slice(-1));
 }
